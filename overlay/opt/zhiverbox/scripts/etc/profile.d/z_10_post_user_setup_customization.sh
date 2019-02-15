fe4541423fb9c2aedc99b50f40e80f8bbddc3632
zciendor <zciendor@mailchuck.com>
2019-02-15 17:13:29 -0600

    replacement of default dropbear key is done in install-zhiverbox.sh now
    see: https://github.com/zhiverbox/armbian-build/issues/7
    Commit ecce27cb4e8e188169825cca58e52e151e08e5b4 achieves the same result in the installer already.
    Therefore we can delete it here.

--- overlay/opt/zhiverbox/scripts/etc/profile.d/z_10_post_user_setup_customization.sh
+++ overlay/opt/zhiverbox/scripts/etc/profile.d/z_10_post_user_setup_customization.sh
@@ -104,54 +104,6 @@ or via it's current (temporary?) IPv4 address:
     fi
 }
 
-replace_default_ssh_boot_key()
-{
-    display_alert "Replace default SSH key for zHIVErbox boot system" "" ""
-    echo -e \
-"We now replace the default SSH key of the zHIVErbox ${RED}boot system${NC}, so you
-you can use just one single SSH key to connect to both - the 'boot system' and
-the 'root system'.
-"
-    press_any_key
-
-    local dropbeardir=/etc/dropbear-initramfs
-
-    # check if the public key is rsa, dsa or ecdsa
-    if ! grep -qE '^([^#]+ )?(ssh-(dss|rsa)|ecdsa-sha2-nistp(256|384|521)) ' "$SSH_USER_KEY"; then
-        display_alert "The public key's signature algorithm is not supported by dropbear/initramfs." "$SSH_USER_KEY" "err"
-        echo -e \
-"You have to manually change the boot system's SSH authentication key:
-    1. Copy a public key with a supported signature alogrithm (RSA, DSA or
-       ECDSA) into a file called ${ORANGE}$dropbeardir/authorized_keys${NC}
-    2. Delete existing 'id_ecdsa' and 'id_ecdsa.pub' keys in that folder
-    3. Manually run ${ORANGE}sudo update-initramfs -uv${NC} to update the
-       boot system (initramfs)
-"
-    else
-        display_alert "Remove default public and private key" "rm $dropbeardir/id_ecdsa*"
-        rm $dropbeardir/id_ecdsa*
-        display_alert "Copy individual public key" "cp $SSH_USER_KEY $dropbeardir"
-        cp $SSH_USER_KEY $dropbeardir
-
-        # update initramfs
-        display_alert "Update boot system (initramfs) to contain new SSH key" "update-initramfs -uv" ""
-        press_any_key
-        update-initramfs -uv
-        echo ""
-        display_alert "Boot system now contains the same SSH public key as the root system." "" "ext"
-
-        local dropbearopts=$(grep DROPBEAR_OPTIONS $dropbeardir/config | sed 's/DROPBEAR_OPTIONS=//; s/"//g')
-        press_any_key
-        # TODO: fix me
-        echo -e \
-"From now on you can login to this zHIVErbox's ${RED}boot system${NC} via it's
-current (temporary?) IPv4 address:
-    ${ORANGE}ssh $dropbearopts root@${ipv4addr}${NC}
-
-Cjdns or Tor access are ${BOLD}not available${NC} on the 'limited' boot system."
-    fi
-}
-
 # main script
 echo ""
 display_alert "Starting zHIVErbox post-user-setup customization..." "" "todo"
@@ -162,9 +114,6 @@ disable_root_account
 
 # enable ssh public key authentication
 enable_ssh_pubkey_for_user
-
-# use the same ssh public key for initramfs authentication as well
-replace_default_ssh_boot_key
 
 echo ""
 display_alert "zHIVErbox post-user-setup customization complete!" "" "ext"
